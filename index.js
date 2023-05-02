6267f9f9941df278051285d17282461abba3b7a7
bossnoyon00 <bossnoyon584@gmail.com>
2023-05-02 09:55:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+
+const chef = require('./data/chef.json');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Chef is coming')
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+
+
+app.listen(port, () => {
+    console.log(`Chef is running ${port}`);
+})
